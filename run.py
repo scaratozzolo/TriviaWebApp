60de51f1c7aee7222b1fb61ad4a6432fd2c44cc3
Scott Caratozzolo <scaratozzolo12@gmail.com>
2021-08-20 16:42:30 -0400

    eventlet and gunicorn

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app import app, socketio
 if __name__ == '__main__':
 
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
-
+    # gunicorn --worker-class eventlet --bind 0.0.0.0:5000  -w 1 run:app
     # app.run(host="0.0.0.0", port=5000, debug=True)
